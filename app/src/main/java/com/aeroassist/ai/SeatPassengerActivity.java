74ea1e3b0312785cdf19dabdb99e541398c64724
TNVILLAN7YT <santhoshb4146.sse@saveetha.com>
2026-07-30 19:31:19 +0530

    fix(android): add missing import android.view.View in SeatPassengerActivity

--- app/src/main/java/com/aeroassist/ai/SeatPassengerActivity.java
+++ app/src/main/java/com/aeroassist/ai/SeatPassengerActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 
